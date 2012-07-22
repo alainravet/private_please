7f7df197f411208591f3bd41aedfe2152c78e572
Alain Ravet <alainravet@gmail.com>
2012-07-22 21:55:06 +0200

    update todo_next_spec.rb

--- spec/todo_next_spec.rb
+++ spec/todo_next_spec.rb
@@ -3,7 +3,40 @@ require 'rspec'
 require 'todo_next'
 
 todo_next(<<TEXT)
-A Foobar
+Single-method usage
+  example :
+    def foo
+      ..
+    end
+    private_please :foo
+
+  * $private_please_candidates == [:foo]
+
+Multi-method usage
+  example :
+    ..
+    private_please :foo, :bar
+
+  - $private_please_candidates == [:foo, :bar]
+
+Global usage
+  example :
+    ..
+    private_please
+      def foo .. end
+      def bar .. end
+
+  - $private_please_candidates == [:foo, :bar]
+
+An outside call to a candidate marked method
+  - goes through as is the method waspublic
+  - $private_please_called_candidates  << the candidate
+  - $private_please_INVALID_candidates << the candidate
+
+An inside call to a candidate marked method
+  - goes through
+  - $private_please_called_candidates << the candidate
+
 	  √ is white by default
 	    ex: puts Foobar.new.colour  # => 'white'
 	  * can be resized
@@ -14,15 +47,17 @@ A Foobar
 	    is always true
 	    is never false
 	  (add more tests)
-  TEXT
+
+Configuration
+  - 'private_please' is inactive by default
+  'private_please' can be activate
+    - via ENV['private_please']=true
+    - PrivatePlease.activate(true)
+
+at_exit
+  - prints a report about the candidates in STDOUT
+
 TEXT
 
 # √ == passed   => same as a comment line
 # * == current  => leading char - '*' - is kept
-# example blocks (ex:, example:) are ignored, like comments.
-
-#describe "<what you're testing>" do
-#  specify 'this should happen' do
-#    .. some code
-#  end
-#end
