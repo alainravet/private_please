9cc4fddcaeacebe84d29bff6ef1af7c7b8177b8f
Alain Ravet <alainravet@gmail.com>
2012-07-22 22:09:14 +0200

    $ todo_next

--- spec/todo_next_spec.rb
+++ spec/todo_next_spec.rb
@@ -3,21 +3,21 @@ require 'rspec'
 require 'todo_next'
 
 todo_next(<<TEXT)
-Single-method usage
+* marking 1 method
   example :
     def foo
       ..
     end
     private_please :foo
 
-  * $private_please_candidates == [:foo]
+  * adds the method to $private_please_candidates
 
-Multi-method usage
+Marking 2 methods in 1 call
   example :
     ..
     private_please :foo, :bar
 
-  - $private_please_candidates == [:foo, :bar]
+  * adds the 2 method to $private_please_candidates
 
 Global usage
   example :
@@ -26,7 +26,7 @@ Global usage
       def foo .. end
       def bar .. end
 
-  - $private_please_candidates == [:foo, :bar]
+  * adds the 2 method to $private_please_candidates
 
 An outside call to a candidate marked method
   - goes through as is the method waspublic
@@ -36,17 +36,6 @@ An outside call to a candidate marked method
 An inside call to a candidate marked method
   - goes through
   - $private_please_called_candidates << the candidate
-
-	  √ is white by default
-	    ex: puts Foobar.new.colour  # => 'white'
-	  * can be resized
-	    example:
-	      foobar.resize!(+10, -2)
-	  - can be reset
-	  truthiness()
-	    is always true
-	    is never false
-	  (add more tests)
 
 Configuration
   - 'private_please' is inactive by default
