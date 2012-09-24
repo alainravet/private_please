5042b3b34fff2c8a94fe187cd51c8e3ddf073cb4
Alain Ravet <alainravet@gmail.com>
2012-09-24 10:38:39 +0200

    repair broken doc alignment

--- lib/private_please/extension.rb
+++ lib/private_please/extension.rb
@@ -14,25 +14,29 @@ module PrivatePlease
       end
 
       methods_to_observe.each do |method_name|
-        PrivatePlease.record_candidate(klass, method_name)
-
-        orig_method = instance_method(method_name)
-
-        define_method(method_name) do |*args, &blk|                                   # def observed_method_i(..)
-          set_trace_func(nil) #don't track activity while here                        #
-                                                                                      #
-          if PrivatePlease.active?                                                    #
-            LineChangeTracker.outside_call_detected?(self) ?                   #
-                PrivatePlease.record_outside_call(self.class, method_name) : #
-                PrivatePlease.record_inside_call( self.class, method_name)   #
-          end                                                                         #
-                                                                                      #
-          set_trace_func(LineChangeTracker::MY_TRACE_FUN)                             #
-          # make the call :                                                           #
-          orig_method.bind(self).call(*args, &blk)                                    #   <call original method>
-        end                                                                           # end
-
+        __instrument_method_for_pp_observation(klass, method_name) # end
       end
+    end
+
+
+    def __instrument_method_for_pp_observation(klass, method_name)
+      PrivatePlease.record_candidate(klass, method_name)
+
+      orig_method = instance_method(method_name)
+
+      define_method(method_name) do |*args, &blk|                           # def observed_method_i(..)
+        set_trace_func(nil) #don't track activity while here                #
+                                                                            #
+        if PrivatePlease.active?                                            #
+          LineChangeTracker.outside_call_detected?(self) ?                  #
+              PrivatePlease.record_outside_call(self.class, method_name) :  #
+              PrivatePlease.record_inside_call(self.class, method_name)     #
+        end                                                                 #
+                                                                            #
+        set_trace_func(LineChangeTracker::MY_TRACE_FUN)                     #
+        # make the call :                                                   #
+        orig_method.bind(self).call(*args, &blk)                            #   <call original method>
+      end                                                                   # end
     end
 
   end
