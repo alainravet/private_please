9acfac66c77e6115794c8b954a059309a63c9938
Alain Ravet <alainravet@gmail.com>
2013-05-02 11:55:08 +0200

    refactor the sample fixtures

--- doc/fixtures/empty_class.rb
+++ doc/fixtures/empty_class.rb
@@ -1,12 +1,5 @@
-$automatic_private_please_tracking = true
-
-begin
-  # for  $ bundle exec ruby -r private_please doc/fixtures/empty_class.r
-  PrivatePlease::Storage
-rescue NameError
-  # for  $ bundle exec ruby -Ilib doc/fixtures/empty_class.r
-  require 'private_please'  
-end
+require File.dirname(__FILE__) + '/fixture_helper.rb'
+load File.dirname(__FILE__) + '/fixture_helper.rb'
 
 module ReportSample
   class EmptyClass
--- doc/fixtures/sample.rb
+++ doc/fixtures/sample.rb
@@ -1,12 +1,4 @@
-$automatic_private_please_tracking = true
-
-begin
-  # for  $ bundle exec ruby -r private_please doc/fixtures/empty_class.r
-  PrivatePlease::Storage
-rescue NameError
-  # for  $ bundle exec ruby -Ilib doc/fixtures/empty_class.r
-  require 'private_please'  
-end
+load File.dirname(__FILE__) + '/fixture_helper.rb'
 
 module ReportSample
                                                 ###########################################
