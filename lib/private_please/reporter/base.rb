edba7f178a7c442a329f75d6987bc45e26f830fa
Alain Ravet <alainravet@gmail.com>
2013-05-04 17:58:38 +0200

    refactoring : Replace Method with Method Object  -  #24

--- lib/private_please/reporter/base.rb
+++ lib/private_please/reporter/base.rb
@@ -1,28 +1,14 @@
+require File.dirname(__FILE__) + '/data_compiler'
 module  PrivatePlease ; module Reporter
 
   class Base
 
-    attr_reader :candidates_store, :calls_store,
-                :good_candidates, :bad_candidates,
-                :good_candidates_c, :bad_candidates_c,
-                :never_called_candidates, :never_called_candidates_c,
-                :building_time
-
+    attr_reader :data
 
     def initialize(candidates_store, calls_store)
-      @candidates_store = candidates_store
-      @calls_store      = calls_store
-
-      start_time = Time.now
-      prepare_report_data
-      @building_time = Time.now - start_time
+      @data = DataCompiler.new(candidates_store, calls_store).compile_data
     end
 
-  private
-
-    def prepare_report_data
-      raise "prepare_report_data() ot implemented in the child class"
-    end
   end
 
 end end
---
+++ lib/private_please/reporter/data_compiler.rb
@@ -0,0 +1,51 @@
+module  PrivatePlease ; module Reporter
+
+  class Data < Struct.new(
+                :candidates_classes_names,
+                :good_candidates,   :bad_candidates,   :never_called_candidates,
+                :good_candidates_c, :bad_candidates_c, :never_called_candidates_c,
+                :building_time
+              )
+  end
+
+  class DataCompiler
+
+    def initialize(candidates_store, calls_store)
+      @candidates_store = candidates_store
+      @calls_store      = calls_store
+    end
+
+    # TODO : optimize (with Hamster?)
+    def compile_data
+      start_time = Time.now
+      bad_candidates   = @calls_store.external_calls      .clone
+      bad_candidates_c = @calls_store.class_external_calls.clone
+      good_candidates  = @calls_store.internal_calls      .clone.remove(bad_candidates)
+      good_candidates_c= @calls_store.class_internal_calls.clone.remove(bad_candidates_c)
+
+      never_called_candidates = @candidates_store.instance_methods.clone.
+          remove(good_candidates).
+          remove(bad_candidates )
+
+      never_called_candidates_c = @candidates_store.class_methods.clone.
+          remove(good_candidates_c).
+          remove(bad_candidates_c )
+      [
+          bad_candidates, bad_candidates_c, good_candidates, good_candidates_c, never_called_candidates, never_called_candidates_c
+      ].each {|arr| arr.reject!{|k, v| v.empty?}}
+
+      candidates_classes_names      = (@candidates_store.instance_methods.classes_names +
+                                       @candidates_store.class_methods   .classes_names ).uniq.sort
+
+      building_time = Time.now - start_time
+      Data.new(
+            candidates_classes_names,
+            good_candidates,   bad_candidates,   never_called_candidates,
+            good_candidates_c, bad_candidates_c, never_called_candidates_c,
+            building_time
+      )
+    end
+
+  end
+
+end end
