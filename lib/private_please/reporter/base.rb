d368bf1a19ae3e474769f5408ebe40276dd4a825
Alain Ravet <alainravet@gmail.com>
2013-05-07 00:33:02 +0200

    introduce class : PrivatePlease::Options + cleanup  -  #30

--- lib/private_please/reporter/base.rb
+++ lib/private_please/reporter/base.rb
@@ -3,10 +3,18 @@ module  PrivatePlease ; module Reporter
 
   class Base
 
-    attr_reader :data
-
     def initialize(candidates_store, calls_store)
       @data = DataCompiler.new(candidates_store, calls_store).compile_data
+    end
+
+  #----------------------------------------------------------------------------
+  # QUERIES:
+  #----------------------------------------------------------------------------
+
+    attr_reader :data
+
+    def options
+      PrivatePlease::Options.current
     end
 
   end
